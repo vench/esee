36136a051c3be785a1fd4860daa2f19cffdaae26
vench <nowwrit@mail.ru>
2016-01-22 15:43:21 +0300

    to class

--- lib.php
+++ lib.php
@@ -2,43 +2,195 @@
 
 namespace esee;
 
-function view($points) {
-	$l = sizeof($points[0]);
-	for($y = 0; $y < sizeof($points); $y ++) {
-		for($x = 0; $x < $l; $x ++) {	 
-			 echo $points[$y][$x] ? '#' : '.';		 	
+/**
+*
+*/
+class Helper {
+
+	/**
+	* @param array $points points [[x,y], [...]]
+	*/
+	public static function view($points) {
+		$l = sizeof($points[0]);
+		for($y = 0; $y < sizeof($points); $y ++) {
+			for($x = 0; $x < $l; $x ++) {	 
+				 echo $points[$y][$x] ? '#' : '.';		 	
+			} 
+			echo "\n";
+		}	
+	}
+
+	/**
+	* @param Chain $c
+	*/
+	public static function view2(Chain $c) { 
+		for($y = 0; $y <= ($c->maxY - $c->minY); $y ++){
+			for($x = 0; $x <= ($c->maxX - $c->minX); $x ++){
+				echo isset($c->p[$y + $c->minY][$x + $c->minX]) ?  '#' : '.';
+			} 
+			echo "\n";
 		} 
-		echo "\n";
-	}	
+	}
+
+
 }
 
-function view2($points) {
-	$xMin = 999999;
-	$yMin = 999999;	
-	$xMax = 0;
-	$yMax = 0;
+/**
+*
+*/
+class ChainBuilder { 
 
-	//print_r($points);
+	private static $see = [];
+
+
+	private $points;
+
+	private $recus = 0;
+
+	private $chain;
+
+
+	public function __construct($points) {
+		$this->points = $points;
+	}
+
+	/**
+	* @retrn int
+	*/
+	public  function getValue($x, $y) {
+		return isset($this->points[$y][$x]) ? $this->points[$y][$x] : 0; 
+	}
+
+	public function hasSee($x, $y)    {
+		if(!isset(self::$see[$y][$x])) {
+			return true;
+		}
+		
+		if($this->recus < 1 && isset($this->chain->p[$y][$x])) {
+			$this->recus ++;
+			return true;
+		}
+		return false;
+	}
+
+	function allowPoint($x, $y) {
+		return $this->getValue($x,$y) == 1 && ( 
+			$this->getValue($x + 1,$y) == 0 ||
+			$this->getValue($x,$y + 1) == 0 ||
+			$this->getValue($x - 1,$y) == 0 ||
+			$this->getValue($x,$y - 1) == 0 ||
+			$this->getValue($x + 1,$y + 1) == 0 ||
+			$this->getValue($x - 1,$y - 1) == 0 ||
+			$this->getValue($x - 1,$y + 1) == 0 ||
+			$this->getValue($x + 1,$y - 1) == 0
+		);
+	}
+
+	/**
+	* @return Chain
+	*/
+	public  function makeChain2($x, $y) {  
+		$this->recus  = 0; //?
+		$this->chain = null;
+		if(isset(self::$see[$y][$x])) {
+			return $this->chain;
+		}
+		if($this->allowPoint($x,$y)) {
+			$this->chain = new Chain(); 
+			$this->chain->addPoint($x, $y);
+			 
+			$cx = $x;
+			$cy = $y;
+			 
+			$n = 0;	
+			$w = 0; 
+			 
+			while(true) { 		 
+				if($n ++ > 0 && $x == $cx && $y == $cy) {
+					break;
+				}
+				//n 
+				if($w < 1 && $this->hasSee($cx,$cy-1) && $this->allowPoint($cx,$cy-1) ) { //echo 'n ';
+					$cy = $cy - 1;
+					self::$see[$cy][$cx] = 1;				
+					$this->chain->addPoint($cx, $cy); 
+					$w = 7;  	
+					continue;
+				}
+				//n&o 
+				if($w < 2 && $this->hasSee($cx+1,$cy-1) && $this->allowPoint($cx+1,$cy-1) ) { //echo 'n&o '; 
+					$cy = $cy - 1;
+					$cx = $cx + 1;
+					self::$see[$cy][$cx] = 1; 
+					$this->chain->addPoint($cx, $cy);
+					$w = 0; 
+					continue;
+				}
+				//o
+				if($w < 3 && $this->hasSee($cx+1,$cy) && $this->allowPoint($cx+1,$cy) ) {// echo 'o ';
+					$cx = $cx + 1;
+					self::$see[$cy][$cx] = 1;				
+					$this->chain->addPoint($cx, $cy);
+					$w = 1;  
+					continue;
+				}
+				//s&o
+				if($w < 4 && $this->hasSee($cx+1,$cy+1) && $this->allowPoint($cx+1,$cy+1) ) { //echo 's&o ';
+					$cx = $cx + 1;
+					$cy = $cy + 1;
+					self::$see[$cy][$cx] = 1;					
+					$this->chain->addPoint($cx, $cy); 
+					$w = 2; 	
+					continue;
+				}
+				//s 
+				if($w < 5 && $this->hasSee($cx,$cy+1) && $this->allowPoint($cx,$cy+1) ) { //echo 's ';
+					$cy = $cy + 1;
+					self::$see[$cy][$cx] = 1; 
+					$this->chain->addPoint($cx, $cy); 
+					$w = 3; 
+					continue;
+				}
+				//s&w 
+				if($w < 6 && $this->hasSee($cx-1,$cy+1) && $this->allowPoint($cx-1,$cy+1) ) { //echo 's&w ';
+					$cy = $cy + 1;
+					$cx = $cx - 1;
+					self::$see[$cy][$cx] = 1; 
+					$this->chain->addPoint($cx, $cy); 
+					$w = 4; 
+					continue;
+				}
+				//w
+				if($w < 7 && $this->hasSee($cx-1,$cy) && $this->allowPoint($cx-1,$cy) ) { //echo 'w ';
+					$cx = $cx - 1;
+					self::$see[$cy][$cx] = 1; 
+					$this->chain->addPoint($cx, $cy); 
+					$w =5; 
+					continue;
+				}
+				//n&w
+				if($w < 8 && $this->hasSee($cx-1,$cy-1) && $this->allowPoint($cx-1,$cy-1) ) {//echo 'n&w '; 
+					$cx = $cx - 1;
+					$cy = $cy - 1;
+					self::$see[$cy][$cx] = 1;				
+					$this->chain->addPoint($cx, $cy); 
+					$w = 6; 
+					continue;
+				}
+				 
+				if($w > 0) {
+					$w = 0; 
+					continue;
+				} 
+				break;
+			} 
+			self::$see[$y][$x] = 1;
+		}
 	
-	foreach($points as $y=>$point) {
-		$yMin = min($yMin, $y);
-		$yMax = max($yMax, $y);
-		foreach($point as $x =>$p) {
-			$xMin = min($xMin, $x);
-			$xMax = max($xMax, $x);
-		}
-	}
-//var_dump($xMin, $xMax);
-	for($y = 0; $y <= ($yMax - $yMin); $y ++){
-		for($x = 0; $x <= ($xMax - $xMin); $x ++){
-			echo isset($points[$y + $yMin][$x + $xMin]) ?  '#' : '.';
-		}
 
-		echo "\n";
+		return $this->chain;
 	}
-
 }
-
 
 /**
 *
@@ -48,8 +200,21 @@ class Chain {
 	public $maxX = null;
 	public $minY = null;
 	public $maxY = null;	
+
+	/**
+	* @var array points [[x,y], [...]]
+	*/
 	public $p = [];
 
+	/**
+	* @var array optimize points [[x,y], [...]]
+	*/
+	public $op = [];
+
+	/**
+	* @param int $x
+	* @param int $y
+	*/
 	public function addPoint($x, $y) {
 		$this->p[$y][$x] = 1;
 		if(is_null($this->minX) || $this->minX > $x) {
@@ -75,6 +240,6 @@ class Cluster {
 }
 
 
-function getValue(&$points, $x, $y) {
-	return isset($points[$y][$x]) ? $points[$y][$x] : 0; 
-}
+
+
+
--- vektr.php
+++ vektr.php
@@ -4,289 +4,17 @@ namespace esee;
 
 require_once './lib.php';
  
+ 
 
-
-
-
-function inChain($x, $y, $x1, $y1, &$points) {
-
-	if(getValue($points,$x,$y) != 1 || 
-		getValue($points,$x1,$y1) != 1) {
-		return false;
-	}
-	if($x == $x1 && abs($y - $y1) == 1 && (
-		getValue($points,$x - 1,$y1) == 0 ||
-		getValue($points,$x - 1,$y ) == 0 ||
-		getValue($points,$x + 1,$y1) == 0 ||
-		getValue($points,$x + 1,$y ) == 0
-		) && (
-1
-	)) {
-		return true;
-	} 
-	if($y == $y1 && abs($x - $x1) == 1 && (
-		getValue($points,$x, $y - 1) == 0 ||
-		getValue($points,$x1, $y + 1) == 0 ||
-		getValue($points,$x, $y - 1) == 0 ||
-		getValue($points,$x1, $y + 1) == 0
-		) && (
-		1  
-)) {
-		return true;
-	}
-	if(abs($y - $y1) == 1 && abs($x - $x1) == 1 && ( 
-		getValue($points,$x1, $y) == 0 ||
-		getValue($points,$x, $y1) == 0 	 
-		)) {
-		return true;
-	}
-	
-	return false;
-}
-
-function makeChain(&$points, $x, $y, &$chain = null) { 
-	static $see = [];
-
-	if(isset($see[$y][$x])) {
-		return $chain;
-	} 
-
-
-	if(getValue($points,$x,$y) == 1 && ( 
-		getValue($points,$x + 1,$y) == 0 ||
-		getValue($points,$x,$y + 1) == 0 ||
-		getValue($points,$x - 1,$y) == 0 ||
-		getValue($points,$x,$y - 1) == 0 ||
-		getValue($points,$x + 1,$y + 1) == 0 ||
-		getValue($points,$x - 1,$y - 1) == 0 ||
-		getValue($points,$x - 1,$y + 1) == 0 ||
-		getValue($points,$x + 1,$y - 1) == 0/**/
-	)) {
-//echo $x . ' ' .$y . ' x' . "\n";
-		if(is_null($chain)) {
-			$chain = [];
-		}
-		$chain[$y][$x] = 1;
-		$see[$y][$x] = 1;
-		//$lx = $x; $ly = $y;
-
-		if(inChain($x, $y, $x + 1, $y, $points)) {
-			makeChain($points, $x + 1, $y, $chain);	
-		} else {
-			//$see[$y][$x + 1] = 1;
-		}
-		if(inChain($x, $y, $x , $y + 1, $points)) {
-			makeChain($points, $x , $y + 1, $chain);	
-		} else {
-			///$see[$y + 1][$x] = 1;
-		}
-		if(inChain($x, $y, $x -1, $y, $points)) {
-			makeChain($points, $x - 1, $y, $chain);	
-		} else {
-			//$see[$y][$x-1] = 1;
-		}
-		if(inChain($x, $y, $x, $y-1, $points)) {
-			makeChain($points, $x, $y-1, $chain);	
-		} else {
-			//$see[$y-1][$x] = 1;
-		}
-		if(inChain($x, $y, $x+1, $y+1, $points)) {
-			makeChain($points, $x+1, $y+1, $chain);	
-		} else {
-			//$see[$y +1][$x+1] = 1;
-		}
-		if(inChain($x, $y, $x-1, $y-1, $points)) {
-			makeChain($points, $x-1, $y-1, $chain);	
-		} else {
-			//$see[$y-1][$x-1] = 1;
-		}
-		if(inChain($x, $y, $x+1, $y-1, $points)) {
-			makeChain($points, $x-1, $y-1, $chain);	
-		} else {
-			//$see[$y-1][$x+1] = 1;
-		}
-		if(inChain($x, $y, $x-1, $y+1, $points)) {
-			makeChain($points, $x-1, $y-1, $chain);	
-		} else {
-			//$see[$y+1][$x-1] = 1;
-		} /*/*
-		makeChain($points, $x + 1, $y, $chain);	
-		makeChain($points, $x - 1, $y, $chain);
-		makeChain($points, $x, $y + 1, $chain);
-		makeChain($points, $x, $y - 1, $chain);
-		makeChain($points, $x + 1, $y + 1, $chain);
-		makeChain($points, $x - 1, $y - 1, $chain);
-		makeChain($points, $x + 1, $y - 1, $chain);
-		makeChain($points, $x - 1, $y + 1, $chain);*/
-	}
-//$see[$y][$x] = 1;
-
-	
-
-	return $chain;
-}
+ 
 
 function optimize($points) {
 	return $points;
 	 
 }
-
-function allowPoint(&$points, $x, $y) {
-	return getValue($points,$x,$y) == 1 && ( 
-		getValue($points,$x + 1,$y) == 0 ||
-		getValue($points,$x,$y + 1) == 0 ||
-		getValue($points,$x - 1,$y) == 0 ||
-		getValue($points,$x,$y - 1) == 0 ||
-		getValue($points,$x + 1,$y + 1) == 0 ||
-		getValue($points,$x - 1,$y - 1) == 0 ||
-		getValue($points,$x - 1,$y + 1) == 0 ||
-		getValue($points,$x + 1,$y - 1) == 0
-	);
-}
-
-
-function makeChain2(&$points, $x, $y, &$chain = null) { 
-	static $see = [];
-	
-	$re  = 0;
-	$hasSee = function($sx,$sy) use(&$chain, &$see, &$re){
-		if(!isset($see[$sy][$sx])) {
-			return true;
-		}
-		
-		if($re < 1 && isset($chain[$sy][$sx])) {
-			$re ++;
-			return true;
-		}
-		return false;
-	};
-
-	if(isset($see[$y][$x])) {
-		return $chain;
-	}
-	if(allowPoint($points,$x,$y)) {
-		if(is_null($chain)) {
-			$chain = [];
-		}
-		$chain[$y][$x] = 1;
-		 
-		$cx = $x;
-		$cy = $y;
-		$lx = $x;
-		$ly = $y;
-		$n = 0;	
-		$w = 0; 
-		$r = 0;
-		while(true) { 
-		 
-			if($n ++ > 0 && $x == $cx && $y == $cy) break;
-		
-			//n 
-			if($w < 1 && $hasSee($cx,$cy-1) && allowPoint($points,$cx,$cy-1) ) {
-				$lx = $cx; $ly = $cy;
-				$see[$cy-1][$cx] = 1;
-				$cy = $cy - 1;
-				$chain[$cy][$cx] = 1; //echo 'n ';
-				$w = 7; 
-				 
-				continue;
-			}
-			//n&o 
-			if($w < 2 && $hasSee($cx+1,$cy-1) && allowPoint($points,$cx+1,$cy-1) ) {
-				$lx = $cx; $ly = $cy;
-				$see[$cy-1][$cx+1] = 1;
-				$cy = $cy - 1;
-				$cx = $cx + 1;
-				$chain[$cy][$cx] = 1; //echo 'n&o '; 
-				$w = 0;
-				 
-				continue;
-			}
-			//o
-			if($w < 3 && $hasSee($cx+1,$cy) && allowPoint($points,$cx+1,$cy) ) {
-				$see[$cy][$cx+1] = 1;
-				$cx = $cx + 1;
-				$chain[$cy][$cx] = 1;// echo 'o ';
-				$w = 1;
-				$lx = $cx; $ly = $cy;
-				continue;
-			}
-			//s&o
-			if($w< 4&& $hasSee($cx+1,$cy+1) && allowPoint($points,$cx+1,$cy+1) ) {
-				$lx = $cx; $ly = $cy;
-				$see[$cy+1][$cx+1] = 1;
-				$cx = $cx + 1;
-				$cy = $cy + 1;	
-				$chain[$cy][$cx] = 1; //echo 's&o ';
-				$w = 2;
-				 		
-				continue;
-			}
-			//s 
-			if($w <5 && $hasSee($cx,$cy+1) && allowPoint($points,$cx,$cy+1) ) {
-				$lx = $cx; $ly = $cy;
-				$see[$cy+1][$cx] = 1;
-				$cy = $cy + 1;
-				$chain[$cy][$cx] = 1; //echo 's ';
-				$w = 3;
-				
-				continue;
-			}
-			//s&w 
-			if($w <6 && $hasSee($cx-1,$cy+1) && allowPoint($points,$cx-1,$cy+1) ) {
-				$lx = $cx; $ly = $cy;
-				$see[$cy+1][$cx-1] = 1;
-				$cy = $cy + 1;
-				$cx = $cx - 1;
-				$chain[$cy][$cx] = 1; //echo 's&w ';
-				$w = 4;
-					
-				continue;
-			}
-			//w
-			if($w<7 && $hasSee($cx-1,$cy) && allowPoint($points,$cx-1,$cy) ) {
-				$lx = $cx; $ly = $cy;
-				$see[$cy][$cx-1] = 1;
-				$cx = $cx - 1;
-				$chain[$cy][$cx] = 1; //echo 'w ';
-				$w =5;
-				
-				continue;
-			}
-			//n&w
-			if($w <8 && $hasSee($cx-1,$cy-1) && allowPoint($points,$cx-1,$cy-1) ) {
-				$lx = $cx; $ly = $cy;
-				$see[$cy-1][$cx-1] = 1;
-				$cx = $cx - 1;
-				$cy = $cy - 1;
-				$chain[$cy][$cx] = 1; //echo 'n&w ';
-				$w = 6;
-				
-				continue;
-			}
-			echo "\n";
-			if($w > 0) {
-				$w = 0;
-
-				
-				continue;
-			}
-			//step last	
-			if($cx != $lx || $cy != $ly) { echo 'change-last';
-				$cx = $lx;
-				$cy = $ly;
-				continue;
-			}
  
-			break;
-		}
-echo "\n";
-		$see[$y][$x] = 1;
-	}
-	
 
-	return $chain;
-}
+
 
 // end functions
 
@@ -362,30 +90,37 @@ $points = [
 	[0,1,1,0,1,1,0,0],
 	[0,1,1,1,1,1,0,0],
 	[0,0,0,1,0,0,0,0],
+];
+$points = [
+	[0,0,0,1,1,1,0,0],
+ 	[0,0,0,0,1,0,0,0],
+	[0,0,0,0,1,0,0,0],
+	[0,0,0,0,1,0,0,0],
 ];*/
 
 
 echo "\n";
-
+$ar = [];
+$chainBuilder = new ChainBuilder($points);
 foreach($points as $y =>$point) {
 	foreach($point as $x => $v) {
 		if($v == 1){
-			$c = makeChain2($points, $x, $y);
+			$c = $chainBuilder->makeChain2($x, $y);
 			if(is_null($c)) {
 				continue;
-			}
-			//print_r($c);
-			 
-			view2($c);/*
-			//print_r($c);
-			foreach($c as $p) {
-				list($x1,$y1) = $p;
-				echo str_repeat('x', $x1 - 303);
-				echo str_repeat('y', $y1 - 190);
-				echo "\n";
-			}*/
+			} 
+			optimize($c);			
+			array_push($ar, $c); 
+		
+			Helper::view2($c); 
+			echo "\n";
 		}
 	}
 }
+
+echo "\n";
+echo sizeof($ar);
+echo "\n";
+
 
 
